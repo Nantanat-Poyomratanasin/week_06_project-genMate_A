25c78e75a7c7262b5c35258508fb28e92988471f
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-04-08 13:44:02 +0700

    add file Car.js

---
+++ Car.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(name,type) {
+    this.name = name;
+    this.type = type; 
+  }
+
+    typeCar() {    
+        console.log(`${this.name} is ${this.type}`)
+  }
+
+  
+}
+module.exports = Car;
